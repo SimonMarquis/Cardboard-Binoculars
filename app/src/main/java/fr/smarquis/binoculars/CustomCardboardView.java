ac8eb52e282ac8877302bea851b695864e77b87d
Simon Marquis <contact@simon-marquis.fr>
2015-10-25 14:42:44 +0100

    Configure the matrix

--- app/src/main/java/fr/smarquis/binoculars/CustomCardboardView.java
+++ app/src/main/java/fr/smarquis/binoculars/CustomCardboardView.java
@@ -1,6 +1,8 @@
 package fr.smarquis.binoculars;
 
 import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
 
 import com.google.vrtoolkit.cardboard.CardboardView;
 import com.google.vrtoolkit.cardboard.Eye;
@@ -11,6 +13,12 @@ import javax.microedition.khronos.egl.EGLConfig;
 
 public class CustomCardboardView extends CardboardView implements CardboardView.StereoRenderer {
 
+    private final float[] mMVPMatrix = new float[16];
+
+    private final float[] mProjectionMatrix = new float[16];
+
+    private final float[] mViewMatrix = new float[16];
+
     public CustomCardboardView(Context context) {
         super(context);
         setEGLContextClientVersion(2);
@@ -20,17 +28,26 @@ public class CustomCardboardView extends CardboardView implements CardboardView.
 
     @Override
     public void onSurfaceCreated(EGLConfig eglConfig) {
-
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
 
     @Override
     public void onSurfaceChanged(int width, int height) {
-
+        GLES20.glViewport(0, 0, width, height);
+        float ratio = (float) width / height;
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     @Override
     public void onNewFrame(HeadTransform headTransform) {
+        // Draw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
     }
 
     @Override
